bcf48e21dd1de644278b5beafa94051dee91e44e
willmarino <w.marino997@gmail.com>
2024-02-04 17:45:46 -0500

    string slice error

--- src/db/scripts/create_test_db.js
+++ src/db/scripts/create_test_db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 (async function() {
-    const dbUrl = process.env.DB_URL.slice(0, indexOf("/lh_test"))
+    const dbUrl = process.env.DB_URL.slice(0, process.env.DB_URL.indexOf("/lh_test"))
     const sequelize = new Sequelize(dbUrl);
 
     await sequelize.query('CREATE DATABASE IF NOT EXISTS lh_test');
