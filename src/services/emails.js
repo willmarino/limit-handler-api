e40ffa13804d9ea40aa918d7d25497840ad95649
willmarino <w.marino997@gmail.com>
2024-06-06 21:59:16 -0400

    small tweaks to email code

--- src/services/emails.js
+++ src/services/emails.js
@@ -1,15 +1,18 @@
+const fs = require("fs");
 const AWS = require("aws-sdk");
 const SES = new AWS.SES({ region: "us-east-1" });
 const bcryptHelpers = require("../helpers/bcrypt");
-const logger = require("../util/logger");
+const { logger } = require("../util/logger");
 
 
 
 const sendOrgInvitationEmail = async (invitationId, senderName, receiverName, orgName, userRoleName) => {
 
-    const invitationIdHash = bcryptHelpers.createHash(invitationId.toString());
+    const invitationIdHash = await bcryptHelpers.createHash(invitationId.toString());
     const acceptURL = `${process.env.SERVER_URL}/invitations/accept?invd=${invitationIdHash}`;
     const logoURL = `${process.env.SERVER_URL}/img/logo/clock_64.png`;
+
+    console.log(logoURL);
 
     await new Promise((resolve, reject) => {
         try{
@@ -26,13 +29,14 @@ const sendOrgInvitationEmail = async (invitationId, senderName, receiverName, or
                             Data:
                                 `
                                 <p> Hello ${receiverName}, </p>
-                                <br><br>
-                                <p> ${senderName} has invited you to join the organization ${orgName}, please follow this link to accept the invitation: </p>
-                                <a href=${acceptURL}></a>
+                                <br>
+                                <a href=${acceptURL}> ${senderName} has invited you to join the organization ${orgName}, please follow this link to accept the invitation </a>
 
                                 <p> Please feel free to contact limithandler@gmail.com with any questions. </p>
+                                
+                                <br>
                                 <img src=${logoURL}></img>
-                                <p> Limit Handler Team </p>
+                                <p> Limit Handler Team :) </p>
 
                                 `
                         }
